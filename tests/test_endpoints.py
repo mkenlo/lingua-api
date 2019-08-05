587e08b3da3f8dc123b90d12af58f8956f5598e0
Melanie <mel.lontsi@gmail.com>
2019-08-05 13:25:04 -0700

    asserted actual value in test functions

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -35,11 +35,12 @@ async def test_getLanguages_with_noParam_shouldPass(test_cli, generateData):
     assert resp_json["total_results"] == 5
     assert len(resp_json["results"]) == 5
     assert resp_json["page"] == 1
+    assert resp_json["results"][0]["name"] == "francais"
     assert resp.status == 200
 
 
-async def test_getLanguages_with_moreThanExpectedParam_shouldShowErrorMsg(test_cli, generateData):
-    params = {'page': 2, 'type': 'local', 'no_valid_param': 50}
+async def test_getLanguages_with_moreThanExpectedParam_shouldFail(test_cli, generateData):
+    params = {'page': 2, 'type': 'local', 'not_valid_param': 50}
     resp = await test_cli.get('/languages', data=json.dumps(params))
     resp_json = await resp.json()
     assert resp_json["message"] == "Expecting less than 3 arguments"
@@ -50,23 +51,23 @@ async def test_getLanguages_with_bothExpectedParam_shouldPass(test_cli, generate
     params = {'page': 2, 'type': 'local'}
     resp = await test_cli.get('/languages', data=json.dumps(params))
     resp_json = await resp.json()
+    assert resp.status == 200
     assert resp_json["total_results"] == 3
     assert len(resp_json["results"]) == 0
     assert resp_json["page"] == 2
-    assert resp.status == 200
 
 
 async def test_getLanguages_with_validPageNum_shouldPass(test_cli, generateData):
     params = {'page': 20}
     resp = await test_cli.get('/languages', data=json.dumps(params))
     resp_json = await resp.json()
+    assert resp.status == 200
     assert resp_json["total_results"] == 5
     assert len(resp_json["results"]) == 0
     assert resp_json["page"] == 20
-    assert resp.status == 200
 
 
-async def test_getLanguages_with_invalidPageNum_shouldShowErrorMsg(test_cli, generateData):
+async def test_getLanguages_with_invalidPageNum_shouldFail(test_cli, generateData):
     params = {'page': "test"}
     resp = await test_cli.get('/languages', data=json.dumps(params))
     resp_json = await resp.json()
@@ -78,6 +79,7 @@ async def test_getLanguages_with_validTypeLanguage_shouldPass(test_cli, generate
     params = {'type': "foreign"}
     resp = await test_cli.get('/languages', data=json.dumps(params))
     resp_json = await resp.json()
+    assert resp.status == 200
     assert resp_json["total_results"] == 2
     assert len(resp_json["results"]) == 2
-    assert resp.status == 200
+    assert resp_json["results"][0]["type"] == "foreign"
